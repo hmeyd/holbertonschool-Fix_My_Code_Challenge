bf5d99dc62b29168b90bac80ecdb8d3a0ec5f2f6
hmeyd <yyahmedsalm@example.com>
2025-05-12 03:25:24 +0200

    task 1

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 /*
     Print a square with the character #
     
@@ -6,22 +6,18 @@
     of the program.
 */
 
+
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
+    process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10);  // Base 10 for decimal input
+size = parseInt(process.argv[2], 10)
 
-if (isNaN(size) || size <= 0) {  // Validation for non-numeric or invalid size
-    process.stderr.write("Invalid size\n");
-    process.exit(1);
-}
-
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
